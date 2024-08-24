c73c82aef66b5212fb8179e8559b122f51872a9d
Sumit Sharma <124554717+sumitsharma1436@users.noreply.github.com>
2024-08-24 18:31:55 +0530

    commit

---
+++ Code.js
@@ -0,0 +1,25 @@
+
+// 1) Write a function to reverse a string in JavaScript.
+
+// function app(str) {
+//     let res = str.split("").reverse().join("");
+//     console.log(res);
+// }
+// app("Hello World")
+
+// 2) How would you check if a string is a palindrome?
+// function app(str) {
+//     let res=str.split("").reverse().join("");
+//     //    this code is case-sensitive Because Dad and dad is different 
+//      if(str==res)
+//      {
+//         console.log("This is Palindrome");
+//      }
+//      else{
+//         console.log("This is Not Palindrome");
+//      }
+// }
+// app("dad")
+
+// 3) Write a function that returns the Fibonacci sequence up to a given number n.
+
