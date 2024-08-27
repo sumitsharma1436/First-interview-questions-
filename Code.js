3941be5983dc5230b58055c24ddcbbdb42a6ae58
Sumit Sharma <124554717+sumitsharma1436@users.noreply.github.com>
2024-08-27 21:41:14 +0530

    Update Code.js

--- Code.js
+++ Code.js
@@ -139,3 +139,25 @@ hu
     //}
 //}
 //app(152)
+// 9) Armstrong number
+
+//function app(num)
+//{
+  //let org=num;
+  //let res=0;
+ // while(num>0)
+   // {
+      //let a=num%10;
+      //num=parseInt(num/10);
+     // res=res+(a*a*a);
+   // }
+  //console.log(res)
+  //if(org===res)
+   // {
+    //  console.log("This number is a Armstrong ")
+   // }
+  //else {
+   // console.log("This number is not Armstrong ")
+ // }
+//}
+//app(153)
